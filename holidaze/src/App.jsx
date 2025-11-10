de7e051c6af593a5924de6bf9f26d0ef7de6efd0
Stine Knutsen <160833015+stineknutsen@users.noreply.github.com>
2025-11-10 19:31:01 +0100

    setup router and test layout, header and footer

--- holidaze/src/App.jsx
+++ holidaze/src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<div>HomePage</div>} />
+        <Route path="/login" element={<div>HomePage</div>} />
+        <Route path="/register" element={<div>RegisterPage</div>} />
+        <Route path="/profile" element={<div>LoginPage</div>} />
+        <Route path="/venues:id" element={<div>VenuePage</div>} />
+        <Route path="*" element={<div>NoPage</div>} />
+      </Route>
+    </Routes>
+  );
 }
-
-export default App
---
+++ holidaze/src/components/Footer/index.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2025 holidaze. All rights reserved.</p>
+    </footer>
+  );
+}
