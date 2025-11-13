4a186c4bc60cc78278bd258884b4d920cf145e0e
Stine Knutsen <160833015+stineknutsen@users.noreply.github.com>
2025-11-13 15:10:29 +0100

    add RegisterPage component file and form components folder

--- holidaze/src/App.jsx
+++ holidaze/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
+import RegisterPage from "./pages/RegisterPage";
 
 export default function App() {
   return (
@@ -8,7 +9,7 @@ export default function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<div>HomePage</div>} />
         <Route path="/login" element={<div>HomePage</div>} />
-        <Route path="/register" element={<div>RegisterPage</div>} />
+        <Route path="/register" element={<RegisterPage />} />
         <Route path="/profile" element={<div>LoginPage</div>} />
         <Route path="/venues:id" element={<div>VenuePage</div>} />
         <Route path="*" element={<div>NoPage</div>} />
