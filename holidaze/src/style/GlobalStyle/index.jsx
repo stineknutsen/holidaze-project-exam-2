8d6737292d8986fe656b7d0a492e0b268db9bd22
Stine Knutsen <160833015+stineknutsen@users.noreply.github.com>
2025-11-20 17:18:03 +0100

    add theme and globalstyle files

---
+++ holidaze/src/style/GlobalStyle/index.jsx
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+* {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+  
+  App {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
+  
+  Main {
+    display: flex;
+    flex: 1;
+    justify-content: center;
+  }
+`;
+
+export default GlobalStyle;
---
+++ holidaze/src/style/Theme/index.jsx
@@ -0,0 +1,36 @@
+import { ThemeProvider } from "styled-components";
+
+const theme = {
+  colors: {
+    text: "#181522",
+    background: "#f8f8f8",
+    primary: "#ede8ff",
+    secondary: "#c4b5fd",
+    tertiary: "#a78bfa",
+    accent: "#6835ff",
+    success: "#c6fbe7",
+    error: "#ffcbcb",
+  },
+  fonts: {
+    main: '"Open Sans", sans-serif',
+    heading: '"Poppins", sans-serif',
+    links: '"Poppins", sans-serif',
+  },
+  fontWeights: {
+    regular: 400,
+    medium: 500,
+    semibold: 600,
+    bold: 700,
+  },
+  fontSizes: {
+    small: "0.875rem",
+    base: "1rem",
+    large: "1.5rem",
+  },
+};
+
+export const Theme = ({ children }) => {
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+};
+
+export default Theme;
